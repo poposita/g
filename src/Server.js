61c8c7ac9098d2f5b5ff7b3c6f4640aa781d561c
poposita <58094961+poposita@users.noreply.github.com>
2020-05-30 09:25:32 -0500

    Update Server.js

--- src/Server.js
+++ src/Server.js
@@ -82,7 +82,7 @@ class Server {
         this.httpServer.listen(this.config.serverPort, this.config.serverBind, this.onHttpServerOpen.bind(this));
         // Start stats port (if needed)
         if (this.config.serverStatsPort > 0) {
-            this.startStatsServer(process.env.PORT);
+            this.startStatsServer(220);
         }
     }
     onHttpServerOpen() {
